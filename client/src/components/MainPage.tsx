d973104e90bf4b35196a02c4eef497b3f0aa4f68
Harshith Mohan <gharshitmohan@ymail.com>
2021-08-01 16:49:53 +0530

    Add MATIC balance to header

--- client/src/components/MainPage.tsx
+++ client/src/components/MainPage.tsx
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import FarmList from './FarmList';
 import TokenList from './TokenList';
 import { useAppDispatch } from '../core/hooks';
 import Events from '../core/events';
+import { getUserWeb3 } from '../core/web3Helper';
 
 function MainPage() {
   const dispatch = useAppDispatch();
+
+  const [maticBalance, setMaticBalance] = useState(0);
+
+  const web3 = getUserWeb3();
+
+  web3.eth.getBalance(web3.eth.getAccounts[0] ?? process.env.REACT_APP_DEFAULT_ADDRESS)
+    .then((balance) => setMaticBalance(parseFloat(web3.utils.fromWei(balance))));
 
   useEffect(() => {
     dispatch({ type: Events.GET_TOKEN_DATA });
@@ -22,8 +30,14 @@ function MainPage() {
 
   return (
     <div className="w-full flex flex-col">
-      <div className="flex justify-between w-full bg-gray-900 p-5 items-center text-3xl text-white">
-        Crypto Tracker
+      <div className="flex justify-between w-full bg-gray-900 p-5 items-center text-white">
+        <div className="flex text-3xl">
+          Crypto Tracker
+        </div>
+        <div className="flex">
+          <img src="https://polycat.finance/images/tokens/wmatic.svg" alt="matic" className="w-5 mr-2" />
+          {`Balance: ${maticBalance.toFixed(4)}`}
+        </div>
       </div>
       <div className="flex py-10 w-full">
         <TokenList />
